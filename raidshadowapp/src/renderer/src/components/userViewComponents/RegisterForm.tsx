44a72891fffad34cf0ce5e3842de512fceca27e8
Luciano Perez <jesus-61914@hotmail.com>
2023-12-17 13:48:39 -0600

    Update registerForm.tsx
    Se cambió el formulario de registro de usuarios

--- raidshadowapp/src/renderer/src/components/userViewComponents/RegisterForm.tsx
+++ raidshadowapp/src/renderer/src/components/userViewComponents/RegisterForm.tsx
@@ -1,76 +1,103 @@
-import { FormEvent, useState, FC } from 'react'
+import { FormEvent, useState, FC, ChangeEvent } from 'react'
 import { NewUser } from '@renderer/types/user.types'
-import { Button } from '@material-tailwind/react'
+import { Button, Input } from '@material-tailwind/react'
+import swal from 'sweetalert2'
 
 interface RegisterFormProps {
   registerHook: () => void
 }
+const currentDate = new Date()
+const dateString = currentDate.toISOString().split('T')[0]
+const initialValues: NewUser = {
+  username: '',
+  register_date: dateString
+}
 
 const RegisterForm: FC<RegisterFormProps> = ({ registerHook }): JSX.Element => {
-  const currentDate = new Date()
-  const dateString = currentDate.toISOString().split('T')[0]
-
-  const [dateValue, setDateValue] = useState<string>(dateString)
-  const [username, setUsername] = useState('')
-
-  const handleUsername = (event: FormEvent<HTMLInputElement>): void => {
-    setUsername(event.currentTarget.value)
-  }
-
-  const handleDate = (event: FormEvent<HTMLInputElement>): void => {
-    setDateValue(event.currentTarget.value)
-  }
-
-  const handleRegister = async (event: FormEvent<HTMLButtonElement>): Promise<void> => {
-    try {
-      event.preventDefault()
-
-      const newUser: NewUser = {
-        username,
-        register_date: dateValue
-      }
-      const result = await window.api.createUser(newUser)
-      if (result.code !== 201) {
-        throw new Error('Error al crear el usuario')
-      }
-      alert('Usuario creado correctamente')
-      registerHook()
-      setUsername('')
-      setDateValue(dateValue)
-    } catch (error) {
-      alert('Error al crear el usuario')
+  const [userValues, setUserValues] = useState<NewUser>(initialValues)
+  const formFields = [
+    {
+      idInput: 1,
+      label: 'Nombre de usuario',
+      type: 'text',
+      name: 'username'
+    },
+    {
+      idInput: 2,
+      label: 'Fecha del registro',
+      type: 'date',
+      name: 'register_date'
     }
+  ]
+
+  const handleValues = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = e.target
+    setUserValues({ ...userValues, [name]: value })
+  }
+
+  const resetValues = (e?: FormEvent<HTMLButtonElement>): void => {
+    if (e) {
+      e.preventDefault()
+    }
+    setUserValues(initialValues)
+  }
+
+  const handleRegister = (e: FormEvent<HTMLButtonElement>): void => {
+    e.preventDefault()
+    const { username, register_date } = userValues
+    const saveUser = async (): Promise<void> => {
+      try {
+        const res = await window.api.createUser({ username, register_date })
+        if (res.code === 201) {
+          swal.fire({
+            title: 'Guardado',
+            text: `${'message' in res ? res.message : ''}`
+          })
+          resetValues()
+          registerHook()
+        }
+        if (res.code !== 201) {
+          swal.fire({
+            title: 'Error',
+            text: `${'error' in res ? res.error : 'Error desconocido'}`
+          })
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    saveUser()
   }
 
   return (
-    <div>
-      <div className="text-center w-1/2 mt-8 mx-auto">
-        <div className="relative h-10 w-full min-w-[200px]">
-          <input
-            value={username}
-            onInput={handleUsername}
-            className="peer bg-white h-full w-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-purple-400 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-            placeholder=" "
-          />
-          <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-purple-400 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-purple-400 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-purple-400 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-            Username
-          </label>
-        </div>
-      </div>
-      <div className="text-center">
-        <input
-          id="registerDate"
-          className="mt-2 bg-white mb-2 h-10 w-1/2 min-w-[200px] rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all"
-          placeholder=" "
-          type="date"
-          value={dateValue}
-          onChange={handleDate}
-        />
-      </div>
-      <div className="text-center">
-        <Button className="mt-2" placeholder={''} onClick={handleRegister}>
-          Registrar
-        </Button>
+    <div className="flex justify-center mt-8">
+      <div className="w-1/2 text-center">
+        <form>
+          <div className="space-y-4 mb-4">
+            {formFields.map(({ name, label, type }, index) => {
+              return (
+                <Input
+                  className="bg-white"
+                  value={userValues[name]}
+                  name={name}
+                  key={index}
+                  label={label}
+                  type={type}
+                  crossOrigin={''}
+                  onChange={handleValues}
+                />
+              )
+            })}
+          </div>
+          <div className="space-x-4">
+            <Button placeholder={''} onClick={handleRegister}>
+              Guardar
+            </Button>
+            <Button placeholder={''} onClick={resetValues}>
+              Reiniciar
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   )
